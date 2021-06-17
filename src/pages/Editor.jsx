961e6291933ddbf8caa9740825f2da3366d82f4f
J-roma <roma.jhosep@gmail.com>
2021-06-16 20:04:39 -0500

    Solve UiUx styles

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react'
 import ReactQuill from 'react-quill'
 import styled from 'styled-components';
 import '../../node_modules/react-quill/dist/quill.snow.css';
 import { uploadArticle } from '../helpers/uploadArticle';
-
+import axios from 'axios';
 
 const TextEditor = styled.div`
     padding: 25px;
@@ -14,7 +13,9 @@ const TextEditor = styled.div`
 `
 
 const Editor = () => {
-
+    const [files, setFiles] = useState("")
+    const [filesTwo, setFilesTwo] = useState("")
+    const [state, setestate] = useState(false)
     const [article, setArticle] = useState({
         title: "",
         content: "",
@@ -24,14 +25,71 @@ const Editor = () => {
         createUserID: "",
         description: "",
         categoryLabel: "",
+        poster: "",
+        img: ""
     })
+
+    const CLOUDINARY_API = 'https://api.cloudinary.com/v1_1/romajs/image/upload'
+    const CLOUDINARY_UPLOAD_PRESET = 'htam81et'  
+
+    const handleSubmitUploadVideos = async () => {
+        console.log(files);
+        const formData = new FormData();
+        formData.append('file', files);
+        formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+        await axios({
+            url: CLOUDINARY_API,
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin',
+                'Access-Control-Allow-Credentials': true,
+            },
+            data: formData
+        })
+        .then(res => {
+            setArticle({
+                ...article,
+                poster: `${res.data.secure_url}`
+            })
+            console.log(res.data.secure_url)
+        }).catch(err => {
+               console.log(err);
+        })
+    }
+
+    const handleSubmitUploadVideosTwo = async () => {
+        const formData = new FormData();
+        formData.append('file', filesTwo);
+        formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+        await axios({
+            url: CLOUDINARY_API,
+            method: 'POST',
+            header: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Origin',
+                'Access-Control-Allow-Credentials': true,
+            },
+            data: formData
+        })
+        .then(res => {
+            setArticle({
+                ...article,
+                img: `${res.data.secure_url}`
+            })
+            console.log(res.data.secure_url)
+        }).catch(err => {
+               console.log(err);
+        })
+    }
 
     const handleTitle = (e) => {
         setArticle({
             ...article,
             title: e.target.value,
         });
-        console.log(article.title);
     }
 
     const handleDescription = (e) => {
@@ -39,11 +97,9 @@ const Editor = () => {
             ...article,
             description: e.target.value,
         });
-        console.log(e.target.value);
     }
 
     const handleCategory = (e) => {
-        console.log(e.target.id);
         setArticle({
             ...article,
             categoryLabel: e.target.id
@@ -51,7 +107,6 @@ const Editor = () => {
     }
 
     const handleBody = (e) => {
-        console.log(e);
         setArticle({
             ...article,
             content: e,
@@ -62,7 +117,23 @@ const Editor = () => {
         uploadArticle(article)
     }
 
+    useEffect(() => {
+        if(files){
+            handleSubmitUploadVideos()
+        }
+    }, [files])
+
+    useEffect(() => {
+        if(filesTwo){
+            handleSubmitUploadVideosTwo()
+        }
+    }, [filesTwo])
+
+
+
+
     return (
+        <>
         <TextEditor className="Editor container-md">
             <h1 className="mb-4" >¡Crear tu articulo!</h1>
             <div class="mb-3 row">
@@ -72,14 +143,22 @@ const Editor = () => {
                 </div>
             </div>
             <div class="mb-3 row">
-                <label  for="description" class="col-sm-2 col-form-label">Descripción:</label>
+                <label for="description" class="col-sm-2 col-form-label">Descripción:</label>
                 <div class="col-sm-10">
                     <input onChange={handleDescription} className="form-control" type="text" placeholder="Descripción..." id="description" />
                 </div>
             </div>
+            <div class="mb-3">
+                <label for="formFileSm" class="form-label">Poster de tu Publicacion:</label>
+                <input onChange={(e) => setFiles(e.target.files[0])} class="form-control form-control-sm" id="formFileSm" type="file" />
+            </div>
+            <div class="mb-3">
+                <label for="formFileSm" class="form-label">Banner de tu publicacion: </label>
+                <input onChange={(e) => setFilesTwo(e.target.files[0])} class="form-control form-control-sm" id="formFileSm" type="file" />
+            </div>
             <div className="mb-3 row">
                 <div className="col-10">
-                <h6>Contenido del Artículo:</h6>
+                    <h6>Contenido del Artículo:</h6>
                 </div>
                 <div class="dropdown col ms-5">
                     <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
@@ -87,7 +166,7 @@ const Editor = () => {
                     </button>
                     <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuButton2">
                         <li ><button id="Noticias" onClick={handleCategory} class="dropdown-item " href="#">Noticia</button></li>
-                        <li ><button id="Recurso"  onClick={handleCategory} class="dropdown-item " href="#">Recurso</button></li>
+                        <li ><button id="Recurso" onClick={handleCategory} class="dropdown-item " href="#">Recurso</button></li>
                         <li ><button id="Articulo" onClick={handleCategory} class="dropdown-item" href="#">Tutorial</button></li>
                         <li><hr class="dropdown-divider" /></li>
                     </ul>
@@ -103,6 +182,7 @@ const Editor = () => {
 
             <button onClick={handleClick} className="btn btn-primary" type="button"> Subir Articulo</button>
         </TextEditor>
+        </>
     )
 }
 
