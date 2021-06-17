3d056b030a5affc5de15ab59f3a121dca2216081
Luis Chávez <luischdu@gmail.com>
2021-06-17 02:42:32 -0500

    adding styles

--- src/pages/VerConocenos.jsx
+++ src/pages/VerConocenos.jsx
@@ -16,26 +16,53 @@ const Img = styled.img`
 const VerConocenos = () => {
   return (
     <Container className="container-md pt-4 bg-white">
-    <div className="row text-center ">
-      <div className="col-md-4 mt-5 ">
-        <Img src="https://media-exp3.licdn.com/dms/image/C4E03AQEVAi3CxxMTPQ/profile-displayphoto-shrink_200_200/0/1621398283381?e=1628726400&v=beta&t=FzVEeF_syvx8SEM6k_u4NZg_VfRB5Ft0LqzEUGjvlEw" alt='Yadier foto'/>
-        <h2>Yadier Caicedo</h2>
-        <p>Desarrollador Front-End</p>
-        <p><a className="btn btn-warning" href="#">Mas Informacion &raquo;</a></p>
+
+      <h2>Equipo de desarrollo</h2>
+      <hr />
+
+      <div className="col-md-12 mt-5 text-center d-flex flex-wrap g-3" data-aos="fade-right">
+        <div>
+          <Img src="https://media-exp3.licdn.com/dms/image/C4E03AQEVAi3CxxMTPQ/profile-displayphoto-shrink_200_200/0/1621398283381?e=1628726400&v=beta&t=FzVEeF_syvx8SEM6k_u4NZg_VfRB5Ft0LqzEUGjvlEw" alt='Yadier foto' />
+          <h2>Yadier Caicedo</h2>
+          <p>Desarrollador Front-End</p>
+          <p><a className="btn btn-warning" href="https://www.linkedin.com/in/yadier-caicedo-150a30180/">Mas Informacion &raquo;</a></p>
+        </div>
+        <div className='d-flex justify-content-center align-items-center'>
+          <p style={{ fontSize: '2vw', color: '#ad38fb' }}>Me gusta viajar, compartir con mi familia, y codear!</p>
+        </div>
       </div>
-      <div className="col-md-4 mt-5">
-        <Img src="https://media-exp3.licdn.com/dms/image/C5603AQEaZ0BUbbPXMw/profile-displayphoto-shrink_100_100/0/1574382153077?e=1629331200&v=beta&t=zp2IMWbEAPud_nzEr6Nti5hOTouFenVsGEwt6psRRZo" alt='Jhosep foto'/>
-        <h2>Jhosep Ropero</h2>
-        <p>Desarrollador Front-End</p>
-        <p><a className="btn btn-warning" href="#">Mas Informacion &raquo;</a></p>
+
+      <div className="col-md-12 d-flex flex-row-reverse flex-wrap g-3">
+        <div>
+          <div className=" mt-5 text-center" data-aos="fade-left">
+            <Img src="https://media-exp3.licdn.com/dms/image/C5603AQEaZ0BUbbPXMw/profile-displayphoto-shrink_100_100/0/1574382153077?e=1629331200&v=beta&t=zp2IMWbEAPud_nzEr6Nti5hOTouFenVsGEwt6psRRZo" alt='Jhosep foto' />
+            <h2>Jhosep Ropero</h2>
+            <p>Desarrollador Front-End</p>
+            <p><a className="btn btn-warning" href="https://www.linkedin.com/in/jhosep-ropero/">Mas Informacion &raquo;</a></p>
+          </div>
+        </div>
+        <div className='d-flex justify-content-center align-items-center'>
+          <p style={{ fontSize: '2vw', color: '#ad38fb' }}>Me apaciona la tecnologia, jugar al baloncesto y descubrir nuevos lugares!</p>
+        </div>
       </div>
-      <div className="col-md-4 mt-5">
-        <Img src="https://media-exp3.licdn.com/dms/image/C4D03AQFoK79E-LLo2g/profile-displayphoto-shrink_200_200/0/1596340619419?e=1628726400&v=beta&t=lcg-vfWugid3x5VC6liiB6wQp7YXlVzkxjJHHSlBVYE" alt='Luis foto' />
-        <h2>Luis Chavéz</h2>
-        <p>Desarrollador Front-End</p>
-        <p><a className="btn btn-warning" href="https://www.linkedin.com/in/luischdu/">Mas Informacion &raquo;</a></p>
+
+
+      <div className="col-md-12 mt-5 text-center d-flex flex-wrap g-3" data-aos="fade-right">
+        <div>
+          <Img src="https://i.imgur.com/fnUkbj1.png" alt='Luis foto' />
+          <h2>Luis Chavéz</h2>
+          <p>Desarrollador Front-End</p>
+          <p><a className="btn btn-warning" href="https://www.linkedin.com/in/luischdu/">Mas Informacion &raquo;</a></p>
+        </div>
+
+
+          <div className="col-lg-10">
+            <p className="lead">Me gusta la sandia, conocer nuevas personas y aprender cosas nuevas...</p>
+          </div>
+       
+
       </div>
-    </div>
+
     </Container>
   );
 };
